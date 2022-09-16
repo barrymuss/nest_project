ab770ddf040cf50e047858c278fe421277fa7a3c
Barry Musaddiq <musaddiqbarry@gmail.com>
2022-09-16 23:48:12 +0700

    setup

---
+++ src/components/layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Layout = () => {
+	return (
+		<div>
+			<div>Layout</div>
+		</div>
+	);
+};
+
+const Portofolio = () => {
+	return (
+		<div>
+			<div>Portofolio</div>
+		</div>
+	);
+};
+
+Layout.Portofolio = Portofolio;
+
+export default Layout;
